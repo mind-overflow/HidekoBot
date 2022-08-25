58963d2653abd2b1c4235d6de0b403bf2f1b73f6
Beatrice Dellacà <hello@beatrice.wtf>
2022-08-25 22:20:05 +0200

    Make bot token a startup argument

--- src/main/java/wtf/beatrice/hidekobot/HidekoBot.java
+++ src/main/java/wtf/beatrice/hidekobot/HidekoBot.java
@@ -10,21 +10,31 @@ import javax.security.auth.login.LoginException;
 public class HidekoBot
 {
     private static Logger logger = new Logger(HidekoBot.class);
+    private static String botToken;
 
     public static void main(String[] args)
     {
+
+        // check if bot token was specified as a startup argument
+        if(args.length < 1)
+        {
+            logger.log("Please specify your bot token!");
+            return;
+        }
+
+        botToken = args[0];
+
         JDABuilder jdaBuilder;
         JDA jda;
 
         try
         {
-            jdaBuilder = JDABuilder.createDefault("");
+            jdaBuilder = JDABuilder.createDefault(botToken);
             jdaBuilder.setActivity(Activity.playing("the piano"));
             jda = jdaBuilder.build();
         } catch (LoginException e)
         {
             logger.log(e.getMessage());
-            return;
         }
 
     }
