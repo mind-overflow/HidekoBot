d2caccf080f5fed511d3770be8d6af563ef4f5ca
Beatrice Dellacà <hello@beatrice.wtf>
2022-11-21 14:44:48 +0100

    Finish uptime string generator

--- src/main/java/wtf/beatrice/hidekobot/commands/slash/BotInfoCommand.java
+++ src/main/java/wtf/beatrice/hidekobot/commands/slash/BotInfoCommand.java
@@ -72,8 +72,33 @@ public class BotInfoCommand
             LocalDateTime now = LocalDateTime.now();
             long uptimeSeconds = ChronoUnit.SECONDS.between(Configuration.getStartupTime(), now);
             Duration uptime = Duration.ofSeconds(uptimeSeconds);
-            uptime.toDaysPart() // todo
-            embedBuilder.addField("Uptime", "seconds: " + uptime, true);
+            long seconds = uptime.toSecondsPart();
+            long minutes = uptime.toMinutesPart();
+            long hours = uptime.toHoursPart();
+            long days = uptime.toDays();
+
+            StringBuilder uptimeStringBuilder = new StringBuilder();
+            if(days == 0)
+            {
+                if(hours == 0)
+                {
+                    if(minutes == 0)
+                    {} else {
+                        uptimeStringBuilder.append(minutes).append("m ");
+                    }
+                } else {
+                    uptimeStringBuilder.append(hours).append("h ");
+                    uptimeStringBuilder.append(minutes).append("m ");
+                }
+            } else {
+
+                uptimeStringBuilder.append(days).append("d ");
+                uptimeStringBuilder.append(hours).append("h ");
+                uptimeStringBuilder.append(minutes).append("m ");
+            }
+            uptimeStringBuilder.append(seconds).append("s ");
+
+            embedBuilder.addField("Uptime", uptimeStringBuilder.toString(), true);
 
 
         }
