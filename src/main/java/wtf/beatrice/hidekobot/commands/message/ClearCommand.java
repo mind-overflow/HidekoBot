ff80e754ff1e635bcfaa1e5d438284a8a4ce98de
Beatrice Dellacà <hello@beatrice.wtf>
2022-11-22 20:42:47 +0100

    Add comment

--- src/main/java/wtf/beatrice/hidekobot/commands/message/ClearCommand.java
+++ src/main/java/wtf/beatrice/hidekobot/commands/message/ClearCommand.java
@@ -67,6 +67,9 @@ public class ClearCommand implements MessageCommand
 
             // edit the message text and attach a button.
             Button dismiss = ClearChat.getDismissButton();
+            // ^ todo: maybe the dismiss button should also delete the original message sent by the user?
+            // todo: but then, we need to differentiate between command type in the database, and store
+            // todo: that message's id too.
             botMessage = botMessage.editMessage(content).setActionRow(dismiss).complete();
 
             // add the message to database.
