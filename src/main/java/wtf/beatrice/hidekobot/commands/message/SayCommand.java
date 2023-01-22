829e19fac39b8725411420387e4a6eba129ddefa
Beatrice Dellacà <hello@beatrice.wtf>
2023-01-22 12:42:35 +0100

    Prevent trying to delete "say" message in DMs

--- src/main/java/wtf/beatrice/hidekobot/commands/message/SayCommand.java
+++ src/main/java/wtf/beatrice/hidekobot/commands/message/SayCommand.java
@@ -1,6 +1,7 @@
 package wtf.beatrice.hidekobot.commands.message;
 
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -53,17 +54,26 @@ public class SayCommand implements MessageCommand
     {
 
         String messageContent;
-        if(args.length != 0 && !args[0].isEmpty())
+        if (args.length != 0 && !args[0].isEmpty())
         {
             messageContent = args[0];
-        } else {
+        } else
+        {
             event.getMessage().reply("\uD83D\uDE20 Hey, you have to tell me what to say!")
                     .queue();
             return;
         }
 
         event.getChannel().sendMessage(messageContent).queue();
-        event.getMessage().delete().queue();
 
+        if (event.getChannel() instanceof TextChannel)
+        {
+            event.getMessage().delete().queue(response -> {
+                // nothing to do with the response
+            }, error -> {
+                // ignore the error if we couldn't delete it, we were probably missing permissions
+                // without this block it would print a stack trace in console
+            });
+        }
     }
 }
